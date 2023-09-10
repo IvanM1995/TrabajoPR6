bd45d3a68f9a77d0108590c79e26c5f1f2678caa
IvanM1995 <142050290+IvanM1995@users.noreply.github.com>
2023-09-10 14:09:55 -0300

    Initial commit

---
+++ src/Tp6/Categoria.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tp6;
+
+/**
+ *
+ * @author Ivan
+ */
+public enum Categoria {
+    COMESTIBLE,
+    LIMPIEZA,
+    PERFUMERIA
+    
+}
